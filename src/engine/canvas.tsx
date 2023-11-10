755c6307e2209bdeb21e9685bfd84c644d9f9649
Enderna <adryanender@gmail.com>
2023-11-10 23:32:50 +0100

    Engine setup

---
+++ src/engine/canvas.tsx
@@ -0,0 +1,19 @@
+import React, { useEffect, useRef } from 'react';
+
+type CanvasProps = {
+    children?: JSX.Element | JSX.Element[];
+};
+
+export default function Canvas({ children }: CanvasProps) {
+    const canvasRef = useRef<HTMLCanvasElement | null>(null);
+
+    useEffect(() => {
+
+    }, [])
+    
+    return (
+      <canvas ref={canvasRef}>
+        {children}
+      </canvas>
+    );
+}
---
+++ src/engine/classes/base.ts
@@ -0,0 +1,7 @@
+abstract class EngineBase{
+    constructor(){
+        
+    }
+}
+
+export default EngineBase;
