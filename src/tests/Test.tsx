1db70ea512446e6f4bda1ce0565d614c470933f6
Enderna <adryanender@gmail.com>
2023-11-11 12:49:33 +0100

    Added vectors and test dir

---
+++ src/tests/Test.tsx
@@ -0,0 +1,14 @@
+import React, { useEffect } from 'react'
+import { Vector3 } from '../engine/vectors'
+
+export default function test() {
+  
+  useEffect(()=>{
+    let vec: Vector3 = new Vector3(1,2,3);
+    console.log(vec);
+  }, [])
+
+  return (
+    <div>test</div>
+  )
+}
