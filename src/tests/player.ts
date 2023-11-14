914077f7476562ef2a65c0c82512c158857542ab
endernan4u <adryanender@gmail.com>
2023-11-14 14:10:13 +0100

    Made the way how to add onFrame layout callback

--- src/tests/player.ts
+++ src/tests/player.ts
@@ -6,24 +6,24 @@ type PlayerParams = {
     atck: number;
     position: Vector2;
     nodes: Node2[];
+    onFrameCall: (delta: number, dis: Player)=>void;
 }
 
 class Player extends Object2d{
     hp: number;
     atck: number;
     tick: number;
+    onFrameCall: (delta: number, dis: Player)=>void;
 
-    constructor({hp, atck, position, nodes}: PlayerParams){
+    constructor({hp, atck, position, nodes, onFrameCall}: PlayerParams){
         super({position, nodes});
         this.hp = hp;
         this.atck = atck;
         this.tick = 0;
+        this.onFrameCall = onFrameCall;
     }
-
     onFrame(delta: number): void {
-        this.rotation += (360 * delta) / 10000;
-        this.position.x += Math.sin(this.tick / 100);
-        this.tick++;
+        this.onFrameCall(delta, this);
     }
 }
 
@@ -36,5 +36,10 @@ export const playerObj = new Player({
         new Node2(25, -25, [2]),
         new Node2(25, 25, [3]),
         new Node2(-25, 25, [0]),
-    ]
+    ],
+    onFrameCall: (delta: number, dis: Player)=>{
+        dis.rotation += (360 * delta) / 10000;
+        dis.position.x += Math.sin(dis.tick / 100);
+        dis.tick++;
+    }
 })
