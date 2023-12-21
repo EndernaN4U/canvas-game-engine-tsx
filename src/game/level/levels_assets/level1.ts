d186ce448e502396d77209349a7ee3b2d6136a9f
endernan4u <adryanender@gmail.com>
2023-12-21 18:50:39 +0100

    Added inside nodes for showcase

--- src/game/level/levels_assets/level1.ts
+++ src/game/level/levels_assets/level1.ts
@@ -4,14 +4,16 @@ import { Node2 } from "../../../engine/classes/assets";
 const level1 = {
     id: 1,
     outside: [
-        new Node2(0, 0),
         new Node2(-500, -250),
         new Node2(-500, 250),
         new Node2(500, 250),
         new Node2(500, -250)
     ],
     inside: [
-        
+        new Node2(-250, -125),
+        new Node2(-250, 125),
+        new Node2(250, 125),
+        new Node2(250, -125)
     ]
 } as LevelType;
 
