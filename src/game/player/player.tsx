54e59d0c5f1bb9f9870fc811bbbe15621ab9904f
Enderna <adryanender@gmail.com>
2023-12-27 22:40:09 +0100

    Added player calcPosition

--- src/game/player/player.tsx
+++ src/game/player/player.tsx
@@ -9,29 +9,48 @@ class Player extends Object2d{
     outside_position: number;
     
     constructor(level: Level){
-        const lvlType = level.level;
-        const middle_ind = Math.floor(lvlType.outside.length / 2) - 1;
-
-        const position = lvlType
-        .outside[middle_ind]
-        .position.clone()           // Clone position of first outside node
-        .translate(level.position)  // Translate it by level object position
-        .translate(
-            Vector2.between(
-                lvlType.outside[middle_ind].position,
-                lvlType.outside[middle_ind + 1].position
-            ).multiplyBy(.5)
-        );                          // Translate it by middle point of 2 nodes 
-
-        super({position});
+        super({position: new Vector2(0,0)});
 
         this.hp = 2;
-
         this.level = level;
+
+        const middle_ind = Math.floor(level.level.outside.length / 2) - 1;
         this.outside_position = middle_ind;
+
+        this.calcPosition();
+
+        window.addEventListener("keypress", (e)=>{
+            switch(e.key){
+                case "a":
+                    this.outside_position--;
+                    break;
+                case "d":
+                    this.outside_position++;
+                    break;
+            }
+            this.calcPosition();
+        })
     }
 
     onFrame(): void {}
+
+    calcPosition(): void{
+        const lvlType = this.level.level;
+        const index = this.outside_position;
+        const second_index = this.outside_position + 1;
+        //  TODO: Check if outside of the level outside length
+
+        this.position = lvlType
+        .outside[index]
+        .position.clone()           // Clone position of first outside node
+        .translate(this.level.position)  // Translate it by level object position
+        .translate(
+            Vector2.between(
+                lvlType.outside[index].position,
+                lvlType.outside[second_index].position
+            ).multiplyBy(.5)
+        );                          // Translate it by middle point of 2 nodes 
+    }
 
     draw(ctx: CanvasRenderingContext2D): void {
         ctx.beginPath();
